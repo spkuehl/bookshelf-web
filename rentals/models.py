8d786999e5d148940af0ba119ee218766aa4d5e1
spkuehl <skuehlhorn@gmail.com>
2018-03-28 18:00:20 -0500

    Added Reservation.open_reservation to track open reservations on books.

--- rentals/models.py
+++ rentals/models.py
@@ -27,6 +27,10 @@ class Reservation(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     book = models.ForeignKey(Book, on_delete=models.CASCADE)
     reserve_date = models.DateField(auto_now_add=True)
+    open_reservation = models.BooleanField(default=True)
+
+    # def place_in_queue(self):
+
 
     def __str__(self):
         return '%s %s' % (self.user, self.reserve_date)
