7685f402058181f2ec25d8ddc9f93f76a5385059
spkuehl <skuehlhorn@gmail.com>
2018-04-03 18:43:34 -0500

    Merged all books.tests.test_views APITestCases to one Class BookTests.

--- books/test/test_views.py
+++ books/test/test_views.py
@@ -6,16 +6,25 @@ from books.models import Book
 import datetime
 
 
-class CreateBookTest(APITestCase):
-    """
-    Tests /books create operations.
-    """
-
+class BookTests(APITestCase):
     def setUp(self):
         self.url = reverse('book-list')
-        self.data = {'title': 'The Golden Compass',
+        self.book_one = Book.objects.create(
+            title='The Golden Compass',
+            author='Phillip Pullman',
+            publication_date=datetime.date.today())
+
+        self.book_two = Book.objects.create(
+            title='The Subtle Knife',
+            author='Phillip Pullman',
+            publication_date=datetime.date.today())
+        self.data = {'title': 'The Amber Spyglass',
                      'author': 'Phillip Pullman',
                      'publication_date': '2013-01-29',
+        }
+        self.new_data = {'title': 'The Northern Lights',
+                         'author':'Phillip Pullman',
+                         'publication_date':'2013-01-29',
         }
         self.superuser = User.objects.create_superuser(
             'sutest',
@@ -31,73 +40,33 @@ class CreateBookTest(APITestCase):
 
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertEqual(Book.objects.count(), 1)
-        self.assertEqual(Book.objects.get().title, 'The Golden Compass')
-        self.assertEqual(Book.objects.get().author, 'Phillip Pullman')
-
-
-class DeleteBookTest(APITestCase):
-    """
-    Tests /books delete operations.
-    """
-
-    def setUp(self):
-        self.book_one = Book.objects.create(
-            title='The Golden Compass',
-            author='Phillip Pullman',
-            publication_date=datetime.date.today())
-
-        self.book_two = Book.objects.create(
-            title='The Subtle Knife',
-            author='Phillip Pullman',
-            publication_date=datetime.date.today())
-
-        self.superuser = User.objects.create_superuser(
-            'sutest',
-            'sutest@supass.com',
-            'supass'
-        )
-
-        self.client.login(username='sutest', password='supass')
+        self.assertEqual(Book.objects.count(), 3)
+        self.assertEqual(Book.objects.get(id=3).title, 'The Amber Spyglass')
+        self.assertEqual(Book.objects.get(id=3).author, 'Phillip Pullman')
 
     def test_can_delete_one_book(self):
+        """
+        Ensure we can delete a one Book object.
+        """
         response = self.client.delete(reverse('book-detail', args=[self.book_one.id]))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 1)
 
     def test_can_delete_both_books(self):
+        """
+        Ensure we can delete all Book objects.
+        """
         response = self.client.delete(reverse('book-detail', args=[self.book_one.id]))
         response = self.client.delete(reverse('book-detail', args=[self.book_two.id]))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
         self.assertEqual(Book.objects.count(), 0)
 
-
-class UpdateBookTest(APITestCase):
-    """
-    Tests /books put operations.
-    """
-
-    def setUp(self):
-        self.book = Book.objects.create(
-            title='The Golden Compass',
-            author='Phillip Pullman',
-            publication_date=datetime.date.today())
-        self.superuser = User.objects.create_superuser(
-            'sutest',
-            'sutest@supass.com',
-            'supass'
-        )
-        self.data = {'author':'Phillip Pullman',
-                     'title': 'The Northern Lights',
-                     'publication_date':(datetime.date.today() - datetime.timedelta(120))}
-        self.client.login(username='sutest', password='supass')
-
     def test_can_update_book(self):
-        response = self.client.put(reverse('book-detail', args=[self.book.id]),  self.data)
+        """
+        Ensure we can update a Book object.
+        """
+        response = self.client.put(reverse('book-detail', args=[self.book_one.id]), self.new_data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(Book.objects.get().title, 'The Northern Lights')
+        self.assertEqual(Book.objects.get(id=self.book_one.id).title, 'The Northern Lights')
 
-    def test_can_transition_to_is_not_new(self):
-        self.assertEqual(Book.objects.get().is_new(), True)
-        response = self.client.put(reverse('book-detail', args=[self.book.id]),  self.data)
-        self.assertEqual(Book.objects.get().is_new(), False)
+    # Test conditions in which model methods will be changed through Updates
