44e0fc95af721787b3ef9a082223f8507ab6131d
spkuehl <skuehlhorn@gmail.com>
2018-04-24 21:01:53 -0500

    Tested the book create-rental end point. Closes #26.'

--- books/test/test_api.py
+++ books/test/test_api.py
@@ -4,33 +4,40 @@ import datetime
 from bookshelf.users.models import User
 from rentals.models import Rental
 from django.urls import reverse
+from rest_framework import status
 
 
 class BookAPITest(TestCase):
-    """ Test module for Book Model. """
+    """ Test module for Book API Endpoints. """
 
     def setUp(self):
-        self.url = reverse('book-checkout')
-        Book.objects.create(
+        self.book = Book.objects.create(
             id=1,
             title='Game of Thrones',
             author='George RR Martin',
             publication_date=datetime.date.today()
         )
-        Book.objects.create(
-            id=2,
-            title='Game of Thrones',
-            author='George RR Martin',
-            publication_date=datetime.date.today() - datetime.timedelta(100)
-        )
-        User.objects.create_user(
+        User.objects.create_superuser(
             'utest',
+            'super@test',
             'upass'
         )
-        self.client.login(username='sutest', password='supass')
+        self.client.login(username='utest', password='upass')
 
     def test_checkout_book_endpoint(self):
         """
         Ensure we can create a rental from the book-checkout endpoint.
         """
-        response = self.client.post(self.url, self.book, format='json')
+        url = reverse('book-checkout', kwargs={'pk': self.book.id})
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_can_not_checkout_book_already_rented_endpoint(self):
+        """
+        Ensure we can not create a rental (already out) from the book-checkout endpoint.
+        """
+        self.book.is_rented = True
+        url = reverse('book-checkout', kwargs={'pk': self.book.id})
+        response = self.client.post(url)
+        response = self.client.post(url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
